09c9607c3e6369463dccfb1cd13504c0ae643b03
Simon Zettergren <simon.zettergren@elev.ga.ntig.se>
2023-12-13 14:03:25 +0100

    alla funktioner

--- main.js
+++ main.js
@@ -4,3 +4,24 @@ const addition = () => {
 
 	document.getElementById("svar").value = tal1 + tal2;
 };
+
+const subtraktion = () => {
+	const tal1 = parseInt(document.getElementById("tal1").value);
+	const tal2 = parseInt(document.getElementById("tal2").value);
+
+	document.getElementById("svar").value = tal1 - tal2;
+};
+
+const multiplikation = () => {
+	const tal1 = parseInt(document.getElementById("tal1").value);
+	const tal2 = parseInt(document.getElementById("tal2").value);
+
+	document.getElementById("svar").value = tal1 * tal2;
+};
+
+const division = () => {
+	const tal1 = parseInt(document.getElementById("tal1").value);
+	const tal2 = parseInt(document.getElementById("tal2").value);
+
+	document.getElementById("svar").value = tal1 / tal2;
+};
