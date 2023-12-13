f54a0eef0e9982dbe4301600f31949973ab0f8ce
Simon Zettergren <simon.zettergren@elev.ga.ntig.se>
2023-12-13 13:57:16 +0100

    addition funktion

---
+++ main.js
@@ -0,0 +1,6 @@
+const addition = () => {
+	const tal1 = parseInt(document.getElementById("tal1").value);
+	const tal2 = parseInt(document.getElementById("tal2").value);
+
+	document.getElementById("svar").value = tal1 + tal2;
+};
